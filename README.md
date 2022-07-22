6325841449b13eb9decc040652a2b909d2126006
EduAnacleto <eduardo.statistic@gmail.com>
2022-07-21 23:55:32 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Python module to telegram bot comunication
---
+++ bot.py
@@ -0,0 +1,65 @@
+import telegram.ext
+import os
+import json
+
+
+
+def get_credentials(credentials_directory, botname):
+    if os.path.exists(credentials_directory):
+        f = open(credentials_directory + '/' + botname + '.json', 'r')
+        credentials = json.load(f)
+        f.close()
+
+        return credentials['token']
+    else:
+        return False
+
+
+def start(update, context):
+    update.message.reply_text("Hello!")
+
+
+def help(update, context):
+    update.message.reply_text("""
+    The following commands are available:
+
+    /start -> Welcome Message
+    /help -> This Message
+    /content -> Information about OlhoFinanceiro
+    /contact -> Information about contact
+    """)
+
+def content(update, context):
+    update.message.reply_text("We have ...")
+
+def contact(update, context):
+    print('info', context.bot)
+    print('info', context.user_data)
+    print('info', context.chat_data)
+    print('info', context.bot_data)
+    print('info', update)
+
+    update.message.reply_text("Contact Edu")
+
+
+def handle_message(update, context):
+    update.message.reply_text(f"You said {update.message.text}")
+
+
+if __name__ == '__main__':
+   
+    token = get_credentials('../Credentials', 'OlhoFinanceiroBot')
+    updater = telegram.ext.Updater(token, use_context=True)
+    disp = updater.dispatcher
+
+    disp.add_handler(telegram.ext.CommandHandler("start", start))
+    disp.add_handler(telegram.ext.CommandHandler("help", help))
+    disp.add_handler(telegram.ext.CommandHandler("content", content))
+    disp.add_handler(telegram.ext.CommandHandler("contact", contact))
+    disp.add_handler(telegram.ext.MessageHandler(telegram.ext.Filters.text, handle_message))
+
+    updater.start_polling()
+    updater.idle()
+
+
+
